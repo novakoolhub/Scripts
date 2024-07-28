c1c2f8ef037313194dc26c0768047f6eb34ea83b
Nova <168435550+novakoolhub@users.noreply.github.com>
2024-07-28 23:20:48 +0300

    Update UtilityModule.lua

--- Modules/UtilityModule.lua
+++ Modules/UtilityModule.lua
@@ -87,7 +87,7 @@ function Module.Align:Draggable(GuiObject:GuiObject, DragObject:GuiObject, Smoot
 
 	local DragLoop = RunService.RenderStepped:Connect(function()
 		if Holding == true then
-			local DragPosition = InputService:GetMouseLocation() - HoldPosition + Vector2.new(0, 48)
+			local DragPosition = InputService:GetMouseLocation() - HoldPosition + Vector2.new(0, 38)
 			DragUDim = UDim2.fromOffset(DragPosition.X, DragPosition.Y)
 		end
 			
